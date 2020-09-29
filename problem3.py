d451ddf0f697104cab46c4644a5664f10ff695da
Fiona307 <71406797+Fiona307@users.noreply.github.com>
2020-09-29 12:48:02 -0700

    Update problem3.py

--- problem3.py
+++ problem3.py
@@ -26,3 +26,44 @@ Enter an integer=>4
 Enter an integer=>2
 2,4,5 do not form a Pythagorean triple
 """
+
+a = input("Enter an integer")
+b = input("Enter an integer")
+c = input("Enter an integer")
+x = float(a)
+y = float(b)
+z = float(c)
+
+if x <= y and x <= z:
+    if y <= z:
+        if x**2 + y**2 == z**2:
+            print(a+ "," + b + "," + c + " form a Pythagorean triple")
+        else:
+            print(a+ "," + b + "," + c + " do not form a Pythagorean triple")
+    else:
+        if x**2 + z**2 == y**2:
+            print(a+ "," + c + "," + b + " form a Pythagorean triple")
+        else:
+            print(a+ "," + c + "," + b + " do not form a Pythagorean triple")
+elif y <= x and y <= z:
+    if x <= z:
+        if x**2 + y**2 == z**2:
+            print(b+ "," + a + "," + c + " form a Pythagorean triple")
+        else:
+            print(b+ "," + a + "," + c + " do not form a Pythagorean triple")
+    else:
+        if y**2 + z**2 == x**2:
+            print(b+ "," + c + "," + a + " form a Pythagorean triple")
+        else:
+            print(b+ "," + c + "," + a + " do not form a Pythagorean triple")
+else:
+    if x <= y:
+        if x**2 + z**2 == y**2:
+            print(c+ "," + a + "," + b + " form a Pythagorean triple")
+        else:
+            print(c+ "," + a + "," + b + " do not form a Pythagorean triple")
+    else:
+        if y**2 + z**2 == x**2:
+            print(c+ "," + b + "," + a + " form a Pythagorean triple")
+        else:
+            print(c+ "," + b + "," + a + " do not form a Pythagorean triple")
